aff1f433be01813195d57b8eef3995f3f7b889d8
FalenElora <faleneloranuro@gmail.com>
2025-03-09 12:45:43 +0700

    first commit

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = [
+        'nama_supplier',
+        'kontak_supplier',
+        'alamat'
+    ];
 }
--- database/migrations/2025_03_08_050050_create_suppliers_table.php
+++ database/migrations/2025_03_08_050050_create_suppliers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
+                $table->string(column: 'nama_supplier');
+                $table->string(column: 'kontak_supplier');
+                $table->string(column: 'alamat');
             $table->timestamps();
         });
     }
